3d579cd27ccc7c57fb6ec68ed0851b27cb0b8197
eva <evedovgopostol@gmail.com>
2024-11-29 12:03:14 -0700

    creating index.html

---
+++ main.js
@@ -0,0 +1,43 @@
+let answer= prompt('Какое "официальное" название JavaScript?')
+ if (answer=="ECMAScript"){
+    alert("Верно!");
+ }else {
+    alert('Не знаете? ECMAScript!');
+ }
+      
+    
+    
+
+
+
+let value= prompt("Enter number")
+ if (value > 0){
+    alert(1);
+ }else if(value < 0){
+    alert(-1);
+}else if(value == 0){
+    alert(0);
+}
+
+
+
+
+
+let result;
+  if (a + b < 4) 
+  {
+   result = 'Мало';
+ } else {
+   result = 'Много';
+ }
+   {result = (a + b < 4) ? 'Мало' : 'Много'};
+  
+  
+  
+  
+  
+   let message = (login == 'Сотрудник') ? 'Привет' :
+   (login == 'Директор') ? 'Здравствуйте' :
+   (login == '') ? 'Нет логина' :
+   '';
+  
